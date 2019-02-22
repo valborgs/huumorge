e0040ab283cb4b529f314862eb6f813ec8d97644
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-22 19:41:28 +0900

    img save root3

--- huumorge/settings.py
+++ huumorge/settings.py
@@ -153,4 +153,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/var/www/huumorge/humorge/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/huumorge/humorge/static/media/'
