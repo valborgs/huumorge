c866f16c1dcb24402eef0d328bc6c4cb236bb41d
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-22 22:10:53 +0900

    img resize

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -10,3 +11,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 320 or img.width > 320:
+            output_size = (320,320)
+            img.thumbnail(output_size)
+            img.save()
