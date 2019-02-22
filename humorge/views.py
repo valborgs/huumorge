eea444508e885ee63e9d49067500ebc24f21482f
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-22 21:41:44 +0900

    views models coding: utf-8

--- humorge/views.py
+++ humorge/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from humorge.models import FreeBoard, HumorBoard, FreeComment, HumorComment
