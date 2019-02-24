3ebc3a1e25aa801783d2c5d51452f6a3e383d447
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-24 15:41:00 +0900

    models.py

--- humorge/models.py
+++ humorge/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from tinymce import HTMLField
+from tinymce.models import HTMLField
 
 # Create your models here.
 
