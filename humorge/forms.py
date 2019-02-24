648ba9999fa5f4fb0c98d1c49a6f1837b370f27a
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-24 14:42:19 +0900

    tinymce in post2

--- humorge/forms.py
+++ humorge/forms.py
@@ -4,20 +4,18 @@ from humorge.models import FreeBoard, HumorBoard, FreeComment, HumorComment
 
 
 class FreePostForm(forms.ModelForm):
-    content = forms.CharField(max_length=None, widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
 
     class Meta:
         model = FreeBoard
         fields = ['title', 'content']
-        widgets = {'content': TinyMCE()}
 
 class HumorPostForm(forms.ModelForm):
-    content = forms.CharField(max_length=None, widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
 
     class Meta:
         model = HumorBoard
         fields = ['title', 'content']
-        widgets = {'content': TinyMCE()}
 
 class FreeCommentForm(forms.ModelForm):
 
