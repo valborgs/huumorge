7757c2af9b4926e3ed3693c09553a1813215db1f
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-28 00:10:04 +0900

    urls.py

--- huumorge/urls.py
+++ huumorge/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
 
 
 urlpatterns = [
