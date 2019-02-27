c7c666a29cad8159ee36c5c6fb1e6ca5d420afe4
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-27 23:51:42 +0900

    pw_reset8

--- users/urls.py
+++ users/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     path('password_reset/done', auth_views.PasswordResetDoneView.as_view(
         template_name='users/password_reset_done.html'), name='password_reset_done'
         ),
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'
+        ),
 ]
