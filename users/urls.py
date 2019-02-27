1cbadb27029df0e1b9e97a9e219e70611cebc66e
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-27 23:02:44 +0900

    pw_reset5

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('password_reset', auth_views.PasswordResetView.as_view(
         template_name='users/password_reset.html'), name='password_reset'
         ),
-    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+    path('password_reset_confirm/<slug:uidb64>/<slug:token>', auth_views.PasswordResetConfirmView.as_view(
         template_name='users/password_reset_confirm.html'), name='password_reset_confirm'
         ),
     path('password_reset/done', auth_views.PasswordResetDoneView.as_view(
