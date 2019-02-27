a923479c7ad1b3bbbf3c17622a294cb9cbf7ff6f
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-28 00:14:22 +0900

    pw_reset9

--- huumorge/urls.py
+++ huumorge/urls.py
@@ -16,12 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
+    path('password_reset', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'
+        ),
+    path('password_reset_confirm/<slug:uidb64>/<slug:token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'
+        ),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'
+        ),
+    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'
+        ),
     path('', include('humorge.urls')),
     path('', include('users.urls')),
 ]
