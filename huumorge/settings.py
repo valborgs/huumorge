41c4d59f5715b9b92114b6a3086856a07ddfccb4
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-28 00:20:57 +0900

    pw_reset10

--- huumorge/settings.py
+++ huumorge/settings.py
@@ -162,5 +162,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'voll1212@gmail.com'
+EMAIL_HOST_PASSWORD = 'alstlr1307!'
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, re_path
-from django.contrib.auth import views as auth_views
 from .views import register, login, logout, myinfo
 
 app_name = 'users'
@@ -9,16 +8,5 @@ urlpatterns = [
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
     path('myinfo/<int:pk>', myinfo, name='myinfo'),
-    path('password_reset', auth_views.PasswordResetView.as_view(
-        template_name='users/password_reset.html'), name='password_reset'
-        ),
-    path('password_reset_confirm/<slug:uidb64>/<slug:token>', auth_views.PasswordResetConfirmView.as_view(
-        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'
-        ),
-    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(
-        template_name='users/password_reset_done.html'), name='password_reset_done'
-        ),
-    path('password_reset_complete', auth_views.PasswordResetCompleteView.as_view(
-        template_name='users/password_reset_complete.html'), name='password_reset_complete'
-        ),
+
 ]
