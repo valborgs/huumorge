6a6f7baed7f2068f556e4f30e87ce7df454ab418
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-27 22:38:50 +0900

    pw_reset3

--- huumorge/settings.py
+++ huumorge/settings.py
@@ -157,3 +157,9 @@ STATIC_ROOT = '/var/www/huumorge/humorge/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/huumorge/humorge/static/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
