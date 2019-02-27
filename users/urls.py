503017e9e562cad2c72f207bf80742b960d789d2
valborgs <45898059+valborgs@users.noreply.github.com>
2019-02-27 22:11:27 +0900

    pw_reset

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 from .views import register, login, logout, myinfo
 
 app_name = 'users'
@@ -8,4 +9,6 @@ urlpatterns = [
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
     path('myinfo/<int:pk>', myinfo, name='myinfo'),
+    path('password_reset', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 ]
