048c8232371c76fa71355b2998244ddb33288890
valborgs <45898059+valborgs@users.noreply.github.com>
2019-03-11 20:58:02 +0900

    post_search2

--- humorge/views.py
+++ humorge/views.py
@@ -165,7 +165,7 @@ def humor_post_comment(request, pk):
 
 def post_search(request):
     query = request.GET.get('q')
-    results = FreeBoard.objects.filter(Q(title__icontains=query) | Q(content__iontains=query))
+    results = FreeBoard.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
     paginator = Paginator(results, 12)
     page = request.GET.get('page')
     datas = paginator.get_page(page)
